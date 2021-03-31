38b10da6607141ce94488b7fb5bebb6a0a7d7b68
Igor Amantino <justin.amantino@gmail.com>
2021-03-31 16:27:39 -0300

    finalizando hook

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -71,9 +71,19 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const updateCart=[...cart]
+      const productExists = updateCart.findIndex(product => product.id === productId);
+
+      if(productExists >= 0) {
+        updateCart.splice(productExists, 1)
+        setCart(updateCart);
+
+        localStorage.setItem('@RocketShoes:cart',JSON.stringify(updateCart))
+      }else{
+        throw Error()
+      }
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto')
     }
   };
 
@@ -82,9 +92,31 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if(amount <= 0){
+        return;
+      }
+      const stock = await api.get(`/stock/${productId}`);
+
+      const stockAmount = stock.data.amount;
+
+      if(amount > stockAmount){
+        toast.error('Quantidade solicitada fora de estoque')
+        return;
+      }
+        const updateCart = [...cart];
+
+        const productExist = updateCart.find(product => product.id === productId);
+        
+        if(productExist) {
+          productExist.amount = amount;
+          setCart(updateCart);
+          
+          localStorage.setItem('@RocketShoes:cart',JSON.stringify(updateCart))   
+      }else{
+        throw Error()
+      }
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto')
     }
   };
 
